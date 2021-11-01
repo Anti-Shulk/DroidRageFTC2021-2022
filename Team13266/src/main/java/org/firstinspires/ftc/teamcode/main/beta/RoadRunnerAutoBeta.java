66649df69230c33cfb02c50b0c42249f4cae49f1
Your Name <you@example.com>
2021-11-01 15:27:21 -0500

    13266 added comments to roadrunner op mode

--- Team13266/src/main/java/org/firstinspires/ftc/teamcode/main/beta/RoadRunnerAutoBeta.java
+++ Team13266/src/main/java/org/firstinspires/ftc/teamcode/main/beta/RoadRunnerAutoBeta.java
@@ -13,10 +13,27 @@ public class RoadRunnerAutoBeta extends LinearOpMode {
     public void runOpMode() {
         Drive13266 drive = new Drive13266(hardwareMap);
 
-        Trajectory myTrajectroy = drive.trajectoryBuilder(new Pose2d())
+        // When we say new Pose2d() What we're saying is that we want to assume the bot starts at
+        // the center of the field facing straight (we'll discuss the coordinate system later). If
+        // you want to set your starting position to something else, say x: 5, y: -4, at a 90 degree
+        // turn, you would replace new Pose2d() with new Pose2d(5, -4, Math.toRadians(90)).
+        Trajectory myTrajectroy = drive.trajectoryBuilder(new Pose2d(0, 0, 0))
+                // Notice there is no ; this is bc the line doesn't actually end here but we make
+                // it a new line to make it look nice.
+
+                // be sure to check out the list of all of these builder functions
                 .strafeRight(10)
                 .forward(5)
                 .build();
+                // if you try to build this, you will get an error. This is because of a
+                // Path Continuity Exception. The reason for this is that when you go from strafe
+                // right to forward, the robot experiences a sudden turn  which is infinite
+                // acceleration. Even if the robot had rocket boosters, it would still make
+                // a small arc. To be able to follow this path, the bot needs to come to a full
+                // stop before turning.
+                // Read the learnroadrunner guide at
+                // https://learnroadrunner.com/trajectories.html#path-continuity-exception
+                // to learn some ways to fix this.
         waitForStart();
 
         if(isStopRequested()) return;
