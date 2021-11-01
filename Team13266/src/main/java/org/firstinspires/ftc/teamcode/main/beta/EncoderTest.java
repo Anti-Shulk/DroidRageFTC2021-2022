67b7f3abf39c94cec6abf42f0c6d806a0fe2f935
Your Name <you@example.com>
2021-11-01 14:58:31 -0500

    13266 Encoder test op mode was created

---
+++ Team13266/src/main/java/org/firstinspires/ftc/teamcode/main/beta/EncoderTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.main.beta;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Encoder Test", group="Test")
+public class EncoderTest {
+    /* Declare OpMode members. */
+    HardwareBeta robot       = new HardwareBeta(); // use the class created to define a Pushbot's hardware
+    double          clawOffset  = 0.0 ;                  // Servo mid position
+    final double    CLAW_SPEED  = 0.5 ;                 // sets rate to move servo
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+}
