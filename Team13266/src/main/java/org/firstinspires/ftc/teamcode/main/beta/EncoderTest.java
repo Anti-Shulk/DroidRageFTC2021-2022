ac9a2bbae2dbe531792801822ecf23abbbc566e8
justin <justinschwerdtfeger245@gmail.com>
2021-11-07 22:39:40 -0600

    13266 hardware map test but it might mess some things up

--- Team13266/src/main/java/org/firstinspires/ftc/teamcode/main/beta/EncoderTest.java
+++ Team13266/src/main/java/org/firstinspires/ftc/teamcode/main/beta/EncoderTest.java
@@ -1,11 +1,8 @@
 package org.firstinspires.ftc.teamcode.main.beta;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 public class EncoderTest {
     /* Declare OpMode members. */
-    HardwareBeta robot       = new HardwareBeta(); // use the class created to define a Pushbot's hardware
+    HardwareMapBeta robot       = new HardwareMapBeta(); // use the class created to define a Pushbot's hardware
     double          clawOffset  = 0.0 ;                  // Servo mid position
     final double    CLAW_SPEED  = 0.5 ;                 // sets rate to move servo
 
