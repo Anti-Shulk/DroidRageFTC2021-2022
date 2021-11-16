4865bc57140d424ea888d26c82723f743e4e1a80
Your Name <you@example.com>
2021-11-16 09:41:16 -0600

    6901 auton

--- Team6901/src/main/java/Programs/Auton69.java
+++ Team6901/src/main/java/Programs/Auton69.java
@@ -76,7 +76,7 @@ public class Auton69 extends LinearOpMode {
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 1;
+    static final double     DRIVE_SPEED             = 0.9;
     static final double     TURN_SPEED              = 0.5;
 
     @Override
